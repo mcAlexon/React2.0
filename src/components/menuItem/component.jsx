a84fa23d64688dcf4fb9e636202cb0f2ce16dd8a
Alexandr Pavlov <mr.alexdemonp2003@mail.ru>
2024-07-05 17:18:03 +0300

    task-3

--- src/components/menuItem/component.jsx
+++ src/components/menuItem/component.jsx
@@ -1,7 +1,10 @@
+import { Button } from "../button/component";
+
 export const MenuItem = ({menuItem}) => {
     return <div>
                 <div>Name: {menuItem.name}</div>
                 <div>Price: {menuItem.price}</div>
                 <div>Ingredients: {menuItem.ingredients}</div>
-            </div>
+                <Button/>
+            </div>          
 };
